b8d1c6f61638b0da035a8405ce640248f375011c
Kaviraghul <vettrithamil@gmail.com>
2024-03-06 16:22:54 +0530

    app button code refactored

--- lib/presentation/resources/app_buttons.dart
+++ lib/presentation/resources/app_buttons.dart
@@ -7,20 +7,58 @@ class AppButtons {
     required String text,
     required VoidCallback onTap,
     required bool active,
+    required bool showBackgroundColor,
   }) {
     return InkWell(
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: active ? AppColors.lightBlue : AppColors.lightGrey,
+          color: showBackgroundColor
+              ? active
+                  ? AppColors.lightBlue
+                  : AppColors.lightGrey
+              : null,
           borderRadius: const BorderRadius.all(
             Radius.circular(AppValues.s13),
           ),
         ),
         padding: const EdgeInsets.symmetric(
             vertical: AppValues.s10, horizontal: AppValues.s18),
-        child: Text(text),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w700,
+            color: active ? AppColors.iconBlue : AppColors.lightGrey,
+          ),
+        ),
       ),
+    );
+  }
+
+  static InkWell containerButton({
+    required String text,
+    required VoidCallback onTap,
+    required bool active,
+  }) {
+    return getTextButton(
+      text: text,
+      onTap: onTap,
+      active: active,
+      showBackgroundColor: true,
+    );
+  }
+
+  static InkWell textOnlyButton({
+    required String text,
+    required VoidCallback onTap,
+    required bool active,
+  }) {
+    return getTextButton(
+      text: text,
+      onTap: onTap,
+      active: active,
+      showBackgroundColor: false,
     );
   }
 }
